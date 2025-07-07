19834a692890c52dbda52767417ff01518832860
Rishik Racha <rishikracha@The-Two-Piece.local>
2025-07-07 19:32:42 +0530

    initial try for database connection for railway hosting

--- Backend/config/connection.js
+++ Backend/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 function createConnection() {
     mongoose
-          .connect("mongodb://127.0.0.1:27017/ammuseumdb")
+        //   .connect("mongodb://127.0.0.1:27017/ammuseumdb")
+          .connect("${{ MongoDB.MONGO_URL }}")
           .then(()=>console.log("Connected to Database 'ammuseumdb'"))
           .catch(()=>console.log("Could not connect to Database"));
 }
